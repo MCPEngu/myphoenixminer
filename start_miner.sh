a9aee6867c29025fade3315b0a10c109029e6956
MCPEngu <73783232+MCPEngu@users.noreply.github.com>
2021-03-28 16:17:00 +0700

    Add files via upload

---
+++ start_miner.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting ./PhoenixMiner to mine ETH
+#
+
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+./PhoenixMiner -rvram 1 -pool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -pool2 stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 373Ee6DSDgAYNAVSnfgUV99248L6Ab7aUH.4lu87eu2suc4njl -esm 3 -allpools 1 -allcoins 0
