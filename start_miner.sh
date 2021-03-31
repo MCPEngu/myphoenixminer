7a2058cab82ba68c564d391883583bcec544a532
MCPEngu <73783232+MCPEngu@users.noreply.github.com>
2021-03-31 17:28:18 +0700

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -3,4 +3,4 @@
 #
 
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./PhoenixMiner -rvram 1 -pool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -pool2 stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 373Ee6DSDgAYNAVSnfgUV99248L6Ab7aUH.4lu87eu2suc4njl -esm 3 -allpools 1 -allcoins 0
+./CulMiner -rvram 1 -pool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -pool2 stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 373Ee6DSDgAYNAVSnfgUV99248L6Ab7aUH.4lu87eu2suc4njl -esm 3 -allpools 1 -allcoins 0
